930dd067f86211ca3aedc66844ce7fadb7e7493e
tom <tom@tom-hwi.home>
2026-04-19 05:31:36 +0200

    [ADDED]: orchestrator now save to results folder archive containing pcap + info.json

--- orchestrator/Orchestrator.py
+++ orchestrator/Orchestrator.py
@@ -1,5 +1,5 @@
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from pathlib import Path
 from uuid import uuid4
 import signal
@@ -7,6 +7,10 @@ import subprocess
 import socket
 import time
 from threading import Event
+import json
+import zipfile
+from datetime import datetime
+import hashlib
 
 from collector import Collector
 from network import Server
@@ -17,11 +21,12 @@ logger = logging.getLogger(__name__)
 
 @dataclass
 class Experience:
-    local_exp_id: str
-    pcap_exp_path: str
-    univ_exp_id: str | None = None
+    exp_id: str
+    creation_time: str | None
+    pcap_filename: str
+    pcap_sha256_checksum: str | None = None
     kernel_version: str | None = None
-    kernel_system: str | None = None
+    kernel_name: str | None = None
     kernel_release: str | None = None
     machine: str | None = None
 
@@ -36,8 +41,10 @@ class Orchestrator:
     SERVER_PORT = 5573
     SSH_WAKEUP_TIMEOUT = 120 # in seconds
     PLAYBOOK_TIMEOUT = 20 # in seconds
+    INFO_JSON_FILENAME = "info.json"
+    DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
-    def __init__(self, images, pcap_dir="pcaps"):
+    def __init__(self, images, results_dir="results"):
         self.images = images
         self.server = Server(Orchestrator.SERVER_ADDRESS, Orchestrator.SERVER_PORT)
         # events declaration
@@ -48,14 +55,16 @@ class Orchestrator:
         self.agent_running = False
         self.agent = None
         self.agent_connected = False
+        # running experience dataclass 
         self.running_exp = None
-        self.pcap_dir = Path(pcap_dir)
-        if self.pcap_dir.exists():
-            if not self.pcap_dir.is_dir():
-                raise FileExistsError(f"{self.pcap_dir} is not a directory")
+        self.results_dir = Path(results_dir)
+        # making sure pcap and results dir exist
+        if self.results_dir.exists():
+            if not self.results_dir.is_dir():
+                raise FileExistsError(f"{self.results_dir} is not a directory")
         else:
-            logger.info(f"creating pcap directory at {self.pcap_dir}")
-            self.pcap_dir.mkdir(parents=True, exist_ok=True)
+            logger.info(f"creating results directory at {self.results_dir}")
+            self.results_dir.mkdir(parents=True, exist_ok=True)
         signal.signal(signal.SIGINT, self.sigint_handler)
 
     def sigint_handler(self, _, __):
@@ -66,6 +75,11 @@ class Orchestrator:
         logger.info(f"received command: {data}")
         if data["cmd"] == str(CommandType.DONE):
             self.playbook_done.set()
+        if data["cmd"] == str(CommandType.SYSTEM_INFO):
+            self.running_exp.kernel_name = data["result"]["name"]
+            self.running_exp.kernel_version = data["result"]["version"]
+            self.running_exp.kernel_release = data["result"]["release"]
+            self.running_exp.machine = data["result"]["machine"]
 
     def _ssh_is_up(self, timeout=120):
         """Checks if the SSH port is open and accepting connections."""
@@ -137,14 +151,32 @@ class Orchestrator:
                 logger.info('playbook done')
         return True
 
+    def save_experience(self):
+        logger.info("saving experience")
+        with open(str(self.results_dir / self.running_exp.pcap_filename), "rb") as pcap_file:
+            self.running_exp.pcap_sha256_checksum = hashlib.file_digest(pcap_file, "sha256").hexdigest()
+        with open(self.results_dir / Orchestrator.INFO_JSON_FILENAME, "w") as info_file:
+            json.dump(asdict(self.running_exp), info_file, indent=4)
+        json_path = self.results_dir / Orchestrator.INFO_JSON_FILENAME
+        pcap_path = self.results_dir / self.running_exp.pcap_filename
+        with zipfile.ZipFile(str(self.results_dir / f"{self.running_exp.exp_id}.zip"), "w") as res_archive:
+            res_archive.write(str(json_path), arcname=Orchestrator.INFO_JSON_FILENAME)
+            res_archive.write(str(pcap_path), arcname=self.running_exp.pcap_filename)
+        logger.info("removing pcap and info.json")
+        json_path.unlink(missing_ok=True)
+        pcap_path.unlink(missing_ok=True)
+        logger.info("successfully saved experience")
+
     def start(self):
         self.server.start(self.cmd_handler, on_connection=self.on_connect)
         for image in self.images:
-            local_exp_id = uuid4().hex
-            pcap_path = str(self.pcap_dir / f"{local_exp_id}.pcap")
+            exp_id = uuid4().hex
+            pcap_filename = f"{exp_id}.pcap"
+            pcap_path = str(self.results_dir / pcap_filename)
             self.running_exp = Experience(
-                local_exp_id=local_exp_id,
-                pcap_exp_path=pcap_path
+                exp_id=exp_id,
+                pcap_filename=pcap_filename,
+                creation_time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             )
             self.collector.set_output(pcap_path)
             self.vm = VBoxManage(image)
@@ -155,7 +187,8 @@ class Orchestrator:
             if self.send_agent() is False:
                 break
             self.run_agent()
-            self.run_playbook()
+            if self.run_playbook():
+                self.save_experience()
         self.stop()
 
     def stop(self):
